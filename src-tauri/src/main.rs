a355638dc13d17f1988e2a4c8b55e5c5d77be22a
m1m1sha <18262227804@163.com>
2024-05-20 13:10:21 +0800

    ✨ feat: sudo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,12 @@ use crate::invoke::*;
 
 fn main() {
     tracing_subscriber::fmt::init();
+    
+    #[cfg(not(windows))]
+    if !check_sudo() {
+        process::exit(0);
+    }
+
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
@@ -19,4 +25,17 @@ fn main() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[cfg(not(windows))]
+fn check_sudo() -> bool {
+    let is_elevated = privilege::user::privileged();
+    if !is_elevated {
+        let Ok(exe) = current_exe() else {
+            return true;
+        };
+        let mut elevated_cmd = privilege::runas::Command::new(exe);
+        let _ = elevated_cmd.force_prompt(true).gui(true).run();
+    }
+    is_elevated
 }
