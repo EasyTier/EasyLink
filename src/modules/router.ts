3bf5c3394968a346f4698cca82dbb99c53d5334b
m1m1sha <18262227804@163.com>
2024-06-07 09:25:19 +0800

    🐞 fix: router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,11 +1,12 @@
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 import type { UseModule } from '~/types/modules'
 
 export const install: UseModule = (app) => {
   const router = createRouter({
     history: createWebHistory(),
-    extendRoutes: routes => setupLayouts(routes),
+    routes: setupLayouts(routes),
   })
   app.use(router)
 }
